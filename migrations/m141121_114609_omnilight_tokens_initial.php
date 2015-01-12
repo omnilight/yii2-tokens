e101a61de2a398e36e0c37a6646c8e10f2fa445d
Pavel <pavel.agalecky@gmail.com>
2015-01-12 15:16:50 +0300

    Fix for code pages

--- migrations/m141121_114609_omnilight_tokens_initial.php
+++ migrations/m141121_114609_omnilight_tokens_initial.php
@@ -7,6 +7,11 @@ class m141121_114609_omnilight_tokens_initial extends Migration
 {
     public function up()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'ENGINE=InnoDB CHARSET=utf8';
+        }
+
         $this->createTable('{{%tokens}}', [
             'id' => Schema::TYPE_PK,
             'type' => Schema::TYPE_STRING,
@@ -14,7 +19,7 @@ class m141121_114609_omnilight_tokens_initial extends Migration
             'token' => Schema::TYPE_STRING.'(32)',
             'created_at' => Schema::TYPE_DATETIME,
             'expire_at' => Schema::TYPE_DATETIME,
-        ]);
+        ], $tableOptions);
     }
 
     public function down()
